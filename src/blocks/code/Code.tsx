67614a92b8e8c2c1182c9511834cef20e6637de4
khosbilegt <archerdoc13@gmail.com>
2025-09-02 22:24:26 +1000

    fixed codeblock color

--- src/blocks/code/Code.tsx
+++ src/blocks/code/Code.tsx
@@ -12,12 +12,10 @@ type Props = {
 export default function CodeBlockComponent(props: Props) {
   const { language = 'plaintext', code, caption } = props
 
-  console.log(props)
-
   return (
     <section className="">
       <div className="max-w-4xl mx-auto">
-        <pre className="bg-gray-900 dark:bg-gray-800 text-gray-100 p-4 rounded-xl overflow-x-auto border border-gray-200 dark:border-gray-700">
+        <pre className="bg-gray-900 dark:bg-gray-800 p-4 rounded-xl overflow-x-auto border border-gray-200 dark:border-gray-700">
           <code className={`language-${language} text-sm font-mono`}>{code}</code>
         </pre>
         {caption && <div className="mt-2 text-xs text-gray-500 dark:text-gray-400">{caption}</div>}
